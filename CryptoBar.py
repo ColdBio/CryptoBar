ed700bf5329cdeae2d4ec24287880396f0dd3fc5
ColdBio <64978825+ColdBio@users.noreply.github.com>
2022-02-17 15:17:31 +0000

    Update CryptoBar.py

--- CryptoBar.py
+++ CryptoBar.py
@@ -33,19 +33,45 @@ btc = Coin('bitcoin')
 xrp = Coin('ripple')
 eth = Coin('ethereum')
 
+top_tokens = cg.get_coins_markets(vs_currency='usd', per_page=10)
+
+listofTokens = [f"{top_tokens[0]['name']} - ${top_tokens[0]['current_price']}",
+                f"{top_tokens[1]['name']} - ${top_tokens[1]['current_price']}",
+                f"{top_tokens[2]['name']} - ${top_tokens[2]['current_price']}",
+                f"{top_tokens[3]['name']} - ${top_tokens[3]['current_price']}",
+                f"{top_tokens[4]['name']} - ${top_tokens[4]['current_price']}",
+                f"{top_tokens[5]['name']} - ${top_tokens[5]['current_price']}",
+                f"{top_tokens[6]['name']} - ${top_tokens[6]['current_price']}",
+                f"{top_tokens[7]['name']} - ${top_tokens[7]['current_price']}",
+                f"{top_tokens[8]['name']} - ${top_tokens[8]['current_price']}",
+                f"{top_tokens[9]['name']} - ${top_tokens[9]['current_price']}"]
+
+about_text = """
+Author: ColdbBio
+License: MIT License
+Version: v1.0
+Repository:-\nhttps://github.com/ColdBio/CryptoBar
+""".center(20, "0")
 
 class CryptoBarApp(rumps.App):
     def __init__(self):
         super(CryptoBarApp, self).__init__(f"CryptoBar")
         self.about = rumps.MenuItem(title="About", callback=None)
-
+        self.menu = listofTokens
+        self.about_button = rumps.MenuItem(title="About", callback=None)
+        self.menu = [self.about_button]
+        
+    @rumps.clicked("About")
+    def about(self):
+        rumps.alert("CryptoBar", f"{about_text}")
+    
+    
     @rumps.timer(3)
     def sayhi(self, _):
         btc = Coin('bitcoin')
         xrp = Coin('ripple')
         eth = Coin('ethereum')
         self.title = f"BTC: {btc.coin_price} - ETH: {eth.coin_price} - XRP: {xrp.coin_price}"
-        #print(f"BTC: {btc.coin_price} ETH: {eth.coin_price}, XRP: {xrp.coin_price}")
     
 if __name__ == "__main__":
     CryptoBarApp().run()
